21978083566fcd211c3175650e35df28a06eb0b0
RemcoKuipers <remslaap@live.nl>
2026-09-03 09:58:11 +0200

    chore: initialiseer Spring Boot-project en configureer PostgreSQL

---
+++ src/main/java/nl/remcokuipers/pokemoncardcollection/PokemonCardCollectionApplication.java
@@ -0,0 +1,13 @@
+package nl.remcokuipers.pokemoncardcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCardCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonCardCollectionApplication.class, args);
+    }
+
+}
